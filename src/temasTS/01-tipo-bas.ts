17ea11768242b42d63e7081fa16bce8c616001eb
DIEGO aranda diaz <diegoish20@gmail.com>
2025-09-06 10:24:14 -0600

    commit 06/09/2025

---
+++ src/temasTS/01-tipo-bas.ts
@@ -0,0 +1,19 @@
+console.log("Hola mundo");
+
+var num=23;
+let num2=65;
+const num3=22;
+
+let nombres : string
+let numero:number
+let activo:boolean
+
+let sum=num+num2+num3
+console.log("La suma es " + sum)
+
+let matricula: number|string
+
+matricula =3221221
+matricula = 'AADD205HGTRZGA5'
+
+console.log(matricula)
