1b7277a3b0af9939cf8fc37d8179111177509ff6
DIEGO aranda diaz <diegoish20@gmail.com>
2025-09-10 20:51:14 -0600

    Importar y exportar

---
+++ src/temasTS/06-importar-exportar.ts
@@ -0,0 +1,23 @@
+import { type Producto, calcularISV2 } from "./05-desestructuracion-fnciones";
+
+import {Producto, calcularISV2}
+
+const carrito:Producto[]=[
+{
+    desc:"Telefono 1",
+    precio: 1000
+},
+
+{
+    desc:"Telefono 2",
+    precio: 1000
+},
+
+{
+    desc:"Telefono 3",
+    precio: 1000
+}
+]
+
+const [total,isv]=calcularISV2(carrito);
+console.log("Total carrito", total)
